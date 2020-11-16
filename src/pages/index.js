a88e3a9002f33d7584ff7922191884ea2dd626ea
Niklas Wallentin <niklaswallentin@Niklass-MacBook-Pro.local>
2020-11-16 23:41:31 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const IndexPage = () => {
 
   return (
     <Layout>
-      <SEO title="Home" />
+      <SEO title="Free wallpapers" />
       <Sec style={{ paddingTop: "0px" }}>
         <Wrap>
           <GroupGrid>
